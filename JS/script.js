1cd2a19660b976a1c6fc4515ac4894ebac0097d8
annunziatadr99 <anudar99@hotmail.it>
2024-10-17 15:41:24 +0200

    imposto JS

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+const newNum = [];
+const numTombola = [];
+for (let i = 0; i < 76; i++) {
+  numTombola.push = i;
+  return numTombola;
+}
+const createNum = () => {
+  const tombola = document.getElementById("tombola");
+  for (let i = 1; i < 76; i++) {
+    newNum.push([]);
+  }
+
+  const numTombDiv = document.createElement("div");
+  numTombDiv.className = "nums"; //oppure .claaslist.add
+  const numTombH3 = document.createElement("H3");
+  numTombH3.innerText = i;
+};
+window.onload = () => {};
