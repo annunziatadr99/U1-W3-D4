4f260b42cad80de4c2d8daaf7e84a0dbda8c3f50
annunziatadr99 <anudar99@hotmail.it>
2024-11-19 17:05:31 +0100

    ESERCIZIO COMPLETATO

--- JS/script.js
+++ JS/script.js
@@ -4,30 +4,29 @@ const estraiBtn = document.getElementById("estraiBtn");
 const numeriEstratti = [];
 
 function creaTabellone() {
-  for (let i = 1; i <= 76; i++) {
+  for (let i = 1; i <= 90; i++) {
     const cella = document.createElement("div");
     cella.classList.add("cella");
     cella.textContent = i;
-    cella.id = "cella-" + i;
+    cella.id = "cella" + i;
     tabellone.appendChild(cella);
   }
 }
 
 function estraiNumero() {
-  if (numeriEstratti.length >= 76) {
-    alert("Riavvia la pagina per ricominciare ");
-    return;
-  }
-
   let numeroEstratto;
-
+  
   do {
-    numeroEstratto = Math.floor(Math.random() * 76) + 1;
+    numeroEstratto = Math.floor(Math.random() * 90) + 1;
   } while (numeriEstratti.includes(numeroEstratto));
-
+  
   numeriEstratti.push(numeroEstratto);
-
-  const cellaEstratta = document.getElementById("cella-" + numeroEstratto);
+  
+  if (numeriEstratti.length >= 90) {
+    alert("Riavvia la pagina per ricominciare ");
+   
+  }
+  const cellaEstratta = document.getElementById("cella" + numeroEstratto);
   cellaEstratta.classList.add("estratta");
 }
 
